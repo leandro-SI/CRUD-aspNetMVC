1963c79a95678da9ce5dc5a47c3dcea293f6bc46
Leandro-SI <llmanutencao@bol.com.br>
2021-08-07 22:49:22 -0300

    add Data Annotations

--- Crud-MVC/Crud-MVC/Models/Produto.cs
+++ Crud-MVC/Crud-MVC/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace Crud_MVC.Models
     public class Produto
     {
         public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório")]
         public string Descricao { get; set; }
         public int Quantidade { get; set; }
         public int CategoriaId { get; set; }
