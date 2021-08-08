e121f68efc73a644b8776cde2eda9c532746adb0
Leandro-SI <llmanutencao@bol.com.br>
2021-08-07 22:15:49 -0300

    entidade Produto  migrations

--- Crud-MVC/Crud-MVC/Migrations/20210808011451_CREATE_Produto.Designer.cs
+++ Crud-MVC/Crud-MVC/Migrations/20210808011451_CREATE_Produto.Designer.cs
@@ -3,14 +3,16 @@ using Crud_MVC.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Crud_MVC.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210808011451_CREATE_Produto")]
+    partial class CREATE_Produto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Crud-MVC/Crud-MVC/Models/Context.cs
+++ Crud-MVC/Crud-MVC/Models/Context.cs
@@ -9,6 +9,7 @@ namespace Crud_MVC.Models
     public class Context : DbContext
     {
         public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
